cb157da147f94fb7ab14e9a4b881c6b4bf08b41b
niker7315-collab <niker7315@gmail.com>
2026-07-11 20:25:44 +0300

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('padforge', {
-  loadMeta: () => ipcRenderer.invoke('padforge:load-meta'),
-  saveMeta: (jsonStr) => ipcRenderer.invoke('padforge:save-meta', jsonStr),
-  saveAudio: (id, dataUrl) => ipcRenderer.invoke('padforge:save-audio', id, dataUrl),
-  loadAudio: (id, mime) => ipcRenderer.invoke('padforge:load-audio', id, mime),
-  deleteAudio: (id) => ipcRenderer.invoke('padforge:delete-audio', id),
-});
